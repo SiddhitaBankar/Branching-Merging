c99bcee68e48f027c34bbd402316d7d7ea35f1d3
SiddhitaBankar <134680214+SiddhitaBankar@users.noreply.github.com>
2023-05-29 17:50:57 +0530

    uploading java files

---
+++ HelloCode.java
@@ -0,0 +1,14 @@
+public class HelloCode {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java Git");
+		  
+		  
+	
+
+	}
+
+}
+
+https://github.com/SiddhitaBankar/github-web-1.git
